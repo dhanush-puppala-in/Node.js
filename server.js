4f047695cee589294bd5fb2782d90ed630766a13
_D.P_ <149101741+dhanush-puppala-in@users.noreply.github.com>
2025-07-03 15:01:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+    const express = require('express');
+    const app = express();
+    const path = require('path');   
+    //const port = 8000;
+
+    //app.use(express.static(path.join(__dirname, 'public')));
+
+    app.get('/dp', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public'));
+        app.use((path.join(__dirname, 'public')));
+    });
+    app.get('/', (req, res) => {
+        res.write("hello!!!")/*(path.join(__dirname, 'public', 'index.html'))*/;
+        res.end();
+    });
+
+    app.listen(8000)
+    /*
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });*/
+
+
+    
